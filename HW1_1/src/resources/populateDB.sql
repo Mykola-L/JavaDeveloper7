b115aadcfe970c2a625862c3b1f2bb6abf956f5c
Mykola Lavrushko <mykolalavrushko@gmail.com>
2017-12-17 00:47:57 +0200

    Homework-1_1

---
+++ HW1_1/src/resources/populateDB.sql
@@ -0,0 +1,71 @@
+use homework_1_1;
+
+insert into customers values (1, 'Intetics Inc');
+insert into customers values (2, 'Materialise');
+insert into customers values (3, '3Shape Ukraine');
+insert into customers values (4, 'PMI Consult');
+insert into customers values (5, 'Cloud Employee');
+insert into customers values (6, 'SeedRoute');
+
+insert into companies values (1, 'EPAM');
+insert into companies values (2, 'SoftServe');
+insert into companies values (3, 'Luxoft');
+insert into companies values (4, 'InfoPulse');
+insert into companies values (5, 'GlobalLogic');
+insert into companies values (6, 'Ciklum');
+
+insert into skills values (1, 'Java');
+insert into skills values (2, 'C++');
+insert into skills values (3, 'C#');
+insert into skills values (4, 'Python');
+insert into skills values (5, 'JavaScript');
+insert into skills values (6, 'SQL');
+insert into skills values (7, 'PHP');
+insert into skills values (8, 'Scrum');
+insert into skills values (9, 'Tomcat');
+insert into skills values (10, 'Trello');
+
+insert into projects values (1, 'ThousandEyes', 1, 1);
+insert into projects values (2, 'Handy', 2, 2);
+insert into projects values (3, 'Zenefits', 3, 3);
+insert into projects values (4, 'AdRoll', 4, 4);
+insert into projects values (5, 'Lyft', 5, 5);
+insert into projects values (6, 'Stealth gadget', 6, 6);
+
+insert into developers values (1, 'Ivan', 1, 1);
+insert into developers values (2, 'Makar', 2, 2);
+insert into developers values (3, 'Olga', 3, 3);
+insert into developers values (4, 'Vadim', 4, 4);
+insert into developers values (5, 'Vitja', 5, 5);
+insert into developers values (6, 'Slava', 6, 6);
+insert into developers values (7, 'Katja', 1, 1);
+insert into developers values (8, 'Dasha', 2, 2);
+insert into developers values (9, 'Vasil', 3, 3);
+insert into developers values (10, 'Sasha', 4, 4);
+
+insert into developers_skills values (1, 1);
+insert into developers_skills values (1, 2);
+insert into developers_skills values (2, 1);
+insert into developers_skills values (2, 4);
+insert into developers_skills values (2, 7);
+insert into developers_skills values (3, 1);
+insert into developers_skills values (3, 3);
+insert into developers_skills values (3, 10);
+insert into developers_skills values (4, 2);
+insert into developers_skills values (4, 4);
+insert into developers_skills values (5, 8);
+insert into developers_skills values (5, 6);
+insert into developers_skills values (6, 3);
+insert into developers_skills values (6, 9);
+insert into developers_skills values (7, 1);
+insert into developers_skills values (7, 3);
+insert into developers_skills values (7, 7);
+insert into developers_skills values (8, 1);
+insert into developers_skills values (8, 2);
+insert into developers_skills values (8, 3);
+insert into developers_skills values (8, 4);
+insert into developers_skills values (9, 2);
+insert into developers_skills values (9, 10);
+insert into developers_skills values (10, 3);
+insert into developers_skills values (10, 7);
+insert into developers_skills values (10, 1);
